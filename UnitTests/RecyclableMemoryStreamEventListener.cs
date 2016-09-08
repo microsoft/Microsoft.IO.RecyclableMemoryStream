9923f4bfff4b4d6953dc6f1939dcd69790cc71a1
Arik Ter-Galstyan <arikt@microsoft.com>
2016-09-07 17:31:08 -0700

    Making RecyclableMemoryStream.Dispose thread-safe. Adding a test to verify thread-safety.

---
+++ UnitTests/RecyclableMemoryStreamEventListener.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace Microsoft.IO.UnitTests
+{
+    public class RecyclableMemoryStreamEventListener : EventListener
+    {
+        public RecyclableMemoryStreamEventListener()
+        {
+            this.EnableEvents(RecyclableMemoryStreamManager.Events.Write, EventLevel.Verbose);
+        }
+
+        protected override void OnEventWritten(EventWrittenEventArgs eventData)
+        {
+            const int TagIndex = 1;
+            this.EventWritten(eventData.EventId, (string)eventData.Payload[TagIndex]);
+        }
+
+        public virtual void EventWritten(int eventId, string tag)
+        {
+    
+        }
+    }
+}
