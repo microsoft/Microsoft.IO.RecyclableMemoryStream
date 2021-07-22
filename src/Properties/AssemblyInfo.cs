2e75ee13b803d8c4166bc80b12acd71de37f7722
Ben <benmwatson@users.noreply.github.com>
2021-07-22 14:34:26 -0700

    Update to version 2.1.3; remove code analysis for pushes. (#189)
    Co-authored-by: Ben Watson <bewatson@microsoft.com>

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.2.0")]
-[assembly: AssemblyFileVersion("2.1.2.0")]
+[assembly: AssemblyVersion("2.1.3.0")]
+[assembly: AssemblyFileVersion("2.1.3.0")]
 
 [assembly: CLSCompliant(true)]
 
