0456539f32c4cfd6f57ce56c5ba27c3116f464e5
github-action-benchmark <github@users.noreply.github.com>
2023-03-23 17:43:17 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 8650db096e1b22ae63d5071298bba5c88b29b11f

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1679593397492,
+  "repoUrl": "https://github.com/microsoft/Microsoft.IO.RecyclableMemoryStream",
+  "entries": {
+    "Benchmark.Net Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "lechu445@users.noreply.github.com",
+            "name": "Leszek B",
+            "username": "lechu445"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8650db096e1b22ae63d5071298bba5c88b29b11f",
+          "message": "Add continuous benchmarking (#286)\n\n* Add continuous benchmarking\r\n\r\n* add info to readme\r\n\r\n---------\r\n\r\nCo-authored-by: lechu445 <xxx@example.com>",
+          "timestamp": "2023-03-23T10:42:02-07:00",
+          "tree_id": "0cde90123ca2f13a98ea4b00ac118ca6e56d97e6",
+          "url": "https://github.com/microsoft/Microsoft.IO.RecyclableMemoryStream/commit/8650db096e1b22ae63d5071298bba5c88b29b11f"
+        },
+        "date": 1679593395914,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BenchmarkTests.WriteTest.WriteByte",
+            "value": 71862511.66666667,
+            "unit": "ns",
+            "range": "± 304733.98232080264"
+          },
+          {
+            "name": "BenchmarkTests.WriteTest.WriteSpan",
+            "value": 204736578.13636363,
+            "unit": "ns",
+            "range": "± 9648306.495287245"
+          }
+        ]
+      }
+    ]
+  }
+}
