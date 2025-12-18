3b782b54d126826d3e3e16faefdd7abb2f5c2f33
Ben Watson <bewatson@microsoft.com>
2025-12-18 14:50:12 -0800

    Add <IsTrimmable>, add a TrimVerifier.csproj file that consumes library and can trigger warnings.

---
+++ TrimVerifier/Program.cs
@@ -0,0 +1,10 @@
+namespace TrimVerifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            _ = new Microsoft.IO.RecyclableMemoryStreamManager();
+        }
+    }
+}
